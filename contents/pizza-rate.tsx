fcc7e3f7e3aa6be7f96092fcd2e9fbe0cb5c17cd
ZakariaTalhami <zakaria.a.a.talhami@gmail.com>
2023-07-18 22:16:29 +0300

    feat

--- contents/pizza-rate.tsx
+++ contents/pizza-rate.tsx
@@ -1,14 +1,16 @@
 import overlayStyle from "data-text:./overlay.scss"
 import filledPizzaIcon from "data-base64:~assets/pizza.png"
-import { PlasmoGetStyle } from "plasmo"
+import outlinePizzaIcon from "data-base64:~assets/pizza-outline.png"
+import type { PlasmoGetStyle } from "plasmo"
+
+const MAX_PIZZA_SCALE = 0.05
+const MAX_PIZZA_RATING = 5
 
 export const getStyle: PlasmoGetStyle = () => {
   const style = document.createElement("style")
   style.textContent = overlayStyle
   return style
 }
-
-const MAX_PIZZA_SCALE = 0.05
 
 function matchStringInNode(node: Node, exp: RegExp | string) {
   let matchCount = 0
@@ -27,8 +29,38 @@ function matchStringInNode(node: Node, exp: RegExp | string) {
   return matchCount
 }
 
-const PizzaIcon = () => {
-  return <img src={filledPizzaIcon} className="pizzaIcon" />
+type IconVariant = "filled" | "outlined"
+type PizzaIconProp = { variant?: IconVariant }
+const defaultPizzaIconProp: PizzaIconProp = { variant: "filled" }
+const pizzaIconVariantMap: Record<IconVariant, string> = {
+  filled: filledPizzaIcon,
+  outlined: outlinePizzaIcon
+}
+const PizzaIcon = ({
+  variant = "filled"
+}: PizzaIconProp = defaultPizzaIconProp) => {
+  return <img src={pizzaIconVariantMap[variant]} className="pizzaIcon" />
+}
+
+type PizzaRatingIconProp = {
+  rating: number
+}
+const defaultPizzaRatingIconProp: PizzaRatingIconProp = {
+  rating: 1
+}
+const PizzaRatingIcon = ({
+  rating
+}: PizzaRatingIconProp = defaultPizzaRatingIconProp) => {
+  rating = Math.min(rating, 1) * 100
+
+  return (
+    <div className="pizzaRatingIcon">
+      <PizzaIcon variant="outlined" />
+      <div className="pizzaRatingIconTop" style={{ width: `${rating}%` }}>
+        <PizzaIcon />
+      </div>
+    </div>
+  )
 }
 
 const PizzaRatingOverlay = () => {
@@ -37,12 +69,14 @@ const PizzaRatingOverlay = () => {
 
   if (!totalWordCount) return null
 
-  const pizzaRating =
-    Math.min(1, pizzaCount / totalWordCount / MAX_PIZZA_SCALE) * 5
+  const rating = Math.min(1, pizzaCount / totalWordCount / MAX_PIZZA_SCALE)
+  const pizzaRating = rating * MAX_PIZZA_RATING
+  // TODO: Modify title on rating value;
+  const overlayTitle = `Rating ${parseFloat(pizzaRating.toFixed(1))}/${MAX_PIZZA_RATING} Pizzas: ${pizzaCount} pizzas in ${totalWordCount} words.`
 
   return (
-    <div className="overlay" title={`${pizzaCount}/${totalWordCount}`}>
-      {parseFloat(pizzaRating.toFixed(1))}/5 <PizzaIcon />
+    <div className="overlay" title={overlayTitle}>
+      <PizzaRatingIcon rating={rating} />
     </div>
   )
 }
